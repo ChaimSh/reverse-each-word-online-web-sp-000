267b5d3a5b52c8976993379e397de93a51769849
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-03-26 19:26:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(sentence)
-first_array = sentence.split(" ")
-new_array = []
-first_array.each do |sentence|
-new_array << sentence.reverse
-end
-new_array.join(" ")
-end
+#def reverse_each_word(sentence)
+#first_array = sentence.split(" ")
+#new_array = []
+#first_array.each do |sentence|
+#new_array << sentence.reverse
+#end
+#new_array.join(" ")
+#end
 
 def reverse_each_word(sentence)
 first_array = sentence.split(" ")
